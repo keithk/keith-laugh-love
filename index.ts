8393a11f0f5f0fc6a48064f46d6828608c912604
keithkurson <keith@keithkurson.net>
2023-11-15 08:48:42 -0800

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({ logger: true });
+
+// Respond to .well-known/atproto-did
+fastify.get("/.well-known/atproto-did", async (request, reply) => {
+  return { did: "plc:t3ehyucfy7ofylu4spnivvmb" };
+});
+
+// Serve an HTML page at the root
+fastify.get("/", async (request, reply) => {
+  reply.type("text/html").send("<h1>keith. laugh. love.</h1>");
+});
+
+// Start the server
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
