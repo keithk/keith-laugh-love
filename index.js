6e8f3e5bbfd361595f7dcd6c1c3206d257227b79
keithkurson <keith@keithkurson.net>
2023-11-15 08:59:06 -0800

    use port

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ fastify.get("/", async (request, reply) => {
 // Start the server
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    const port = process.env.PORT || 3000;
+    await fastify.listen({ port });
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
